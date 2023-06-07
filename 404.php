baa448f5fa721b1a664205972572eb74e3f40ae5
Noah Miller <bruno.tribst@gmail.com>
2023-06-07 02:56:36 -0300

    ggfhfg

---
+++ 404.php
@@ -0,0 +1,3 @@
+<?php get_header(); ?>
+<p>page not found</p>
+<?php get_footer(); ?>
--- index.php
+++ index.php
@@ -1,9 +1,29 @@
 <?php get_header(); ?>
 
-<div class="container-sm text-center">
+<div class="container-sm">
 
-    <p>sngknsfklgnsdfkg</p>
+<?php
+$args = array(
+    'posts_per_page' => 7 // Limit the number of posts to 7
+);
 
+$query = new WP_Query($args);
+
+if ($query->have_posts()) {
+    while ($query->have_posts()) {
+        $query->the_post(); ?>
+        
+        <div class="container-fluid" id="post">
+            <h1 class="display-6"><?php the_title(); ?></h1>
+            <?php the_content(); ?>
+        </div>
+<?php
+    }
+}
+
+// Restore the original post data
+wp_reset_postdata();
+?>
 
 </div>
     
