8d885909eb3f7864864f6a8676976e2751294ef2
Noah Miller <bruno.tribst@gmail.com>
2023-06-07 03:03:17 -0300

    grg

--- index.php
+++ index.php
@@ -2,28 +2,26 @@
 
 <div class="container-sm">
 
-<?php
-$args = array(
-    'posts_per_page' => 7 // Limit the number of posts to 7
-);
-
-$query = new WP_Query($args);
-
-if ($query->have_posts()) {
-    while ($query->have_posts()) {
-        $query->the_post(); ?>
-        
-        <div class="container-fluid" id="post">
-            <h1 class="display-6"><?php the_title(); ?></h1>
-            <?php the_content(); ?>
-        </div>
-<?php
+    <!-- Post Loop -->
+    <?php
+    $args = array(
+        'posts_per_page' => 7
+    );
+    $query = new WP_Query($args);
+    if ($query->have_posts()) {
+        while ($query->have_posts()) {
+            $query->the_post(); ?>
+            
+            <div class="container-fluid" id="post">
+                <h1 class="display-6"><?php the_title(); ?></h1>
+                <?php the_content(); ?>
+            </div>
+    <?php
+        }
     }
-}
-
-// Restore the original post data
-wp_reset_postdata();
-?>
+    wp_reset_postdata();
+    ?>
+    <!-- End Post Loop -->
 
 </div>
     
