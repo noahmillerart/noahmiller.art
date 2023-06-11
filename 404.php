fec76a88aa3cd3ed757df55edc3096592a1d28a5
Noah Miller <bruno.tribst@gmail.com>
2023-06-11 03:02:17 -0300

    asdsdas

--- 404.php
+++ 404.php
@@ -1,3 +1,14 @@
 <?php get_header(); ?>
-<p>page not found</p>
+
+<div class="container-md">
+    <div class="row">
+        <div class="col-md-8">
+            <p>page not found</p>
+        </div>
+        <div class="col-md-4">
+            <?php get_sidebar(); ?>
+        </div>
+    </div>
+</div>
+
 <?php get_footer(); ?>
--- search.php
+++ search.php
@@ -1,19 +1,34 @@
-<?php
-get_header(); // Include the header template
+<?php get_header(); ?>
 
-if (have_posts()) {
-    while (have_posts()) {
-        the_post();
-        // Display the title and content of each search result
-        ?>
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        <?php the_excerpt(); ?>
-        <?php
-    }
-} else {
-    // Display a message if no search results are found
-    echo '<p>No search results found.</p>';
-}
+<div class="container-md">
+    <div class="row">
+        <div class="col-md-8">          
 
-get_footer(); // Include the footer template
-?>
+            <?php
+
+            if (have_posts()) {
+                while (have_posts()) {
+                    the_post();
+                    // Display the title and content of each search result
+                    ?>
+                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <?php the_excerpt(); ?>
+                    <?php
+                }
+            } else {
+                // Display a message if no search results are found
+                echo '<p>No search results found.</p>';
+            }
+            
+            ?>
+
+        </div>
+        <div class="col-md-4">
+            <?php get_sidebar(); ?>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
+
+
